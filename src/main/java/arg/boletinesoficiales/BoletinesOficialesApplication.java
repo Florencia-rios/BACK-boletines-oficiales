7096d5ea55cd0ec3b3c82e73fbb6f1929293bc3b
Florencia Rios <florencia.rios@MacBook277.local>
2024-05-12 12:55:47 -0300

    Esqueleto del proyecto

---
+++ src/main/java/arg/boletinesoficiales/BoletinesOficialesApplication.java
@@ -0,0 +1,13 @@
+package arg.boletinesoficiales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletinesOficialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoletinesOficialesApplication.class, args);
+	}
+
+}
