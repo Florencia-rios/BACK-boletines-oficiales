ff85d4ec41a98707ef5e5ec64551ca4d5a075f00
ex-cfotech01 <ex-cfotech01@nacionservicios.com.ar>
2024-08-01 01:57:37 -0300

    Actualizaciones de archivo de properties

--- src/main/java/arg/boletinesoficiales/restcontroller/BoletinesOficialesRestController.java
+++ src/main/java/arg/boletinesoficiales/restcontroller/BoletinesOficialesRestController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(name = "/api")
+@RequestMapping("/api")
 public class BoletinesOficialesRestController {
 
     @Autowired
